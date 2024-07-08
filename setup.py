0ac3cd34886023ea5658b7ce2d8ecbc17d7c4a17
Valer <122545522+Valer100@users.noreply.github.com>
2024-07-08 22:58:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ description = open("README.md", "r").read()
 setup(
     name = "sv_ttk_colorizer",
     version = "0.0.6",
+    python_requires = ">=3.8",
+    license = "MIT",
+    author = "Valer100",
+    description = "A module for changing the accent color of @rdbende's Sun Valley ttk theme.",
+    url = "https://github.com/Valer100/Sun-Valley-Theme-Colorizer",
+    project_urls = {
+        "Source": "https://github.com/Valer100/Sun-Valley-Theme-Colorizer",
+        "Issues": "https://github.com/Valer100/Sun-Valley-Theme-Colorizer/issues",
+    },
     packages = find_packages(),
     install_requires = [
         "darkdetect",
@@ -21,5 +30,29 @@ setup(
         ],
     },
     long_description = description,
-    long_description_content_type = "text/markdown"
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: User Interfaces",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = [
+        "sv-ttk",
+        "sv-ttk-colorizer",
+        "sv-ttk-c",
+        "sv_ttk_colorizer",
+        "sv_ttk_c",
+        "theme",
+        "tk",
+        "ttk",
+        "tkinter",
+        "modern",
+        "fluent",
+        "dark-theme",
+        "sun-valley",
+        "windows-11",
+        "winui",
+    ],
 )
