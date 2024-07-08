afc62d428cd50705b27dad8abd25c30a7e5c2b5a
Valer <122545522+Valer100@users.noreply.github.com>
2024-07-08 23:01:59 +0300

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,5 +1,10 @@
 @echo off
-pip install setuptools, wheel, twine
+pip install setuptools
+pip install wheel
+pip install twine
 rmdir /q /s dist
 python setup.py sdist bdist_wheel
 twine upload dist/*
+rmdir /q /s dist
+rmdir /q /s build
+rmdir /q /s sv_ttk_colorizer.egg-info
