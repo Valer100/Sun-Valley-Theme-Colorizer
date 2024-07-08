15f1358853f335d4bbf2ede7943e152e2bbf5f51
Valer <122545522+Valer100@users.noreply.github.com>
2024-07-08 21:51:08 +0300

    Remove shebang

--- sv_ttk_colorizer/__main__.py
+++ sv_ttk_colorizer/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import tkinter as tk, sv_ttk, darkdetect, os, shutil, sys
 from tkinter import ttk, filedialog as fd, messagebox as msg
 from urllib.request import urlretrieve
