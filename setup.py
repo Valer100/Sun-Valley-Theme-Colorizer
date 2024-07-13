a7dc49fb085eef879d8b8f196d9c40fd73f4988d
Valer <122545522+Valer100@users.noreply.github.com>
2024-07-13 18:36:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ description = open("README.md", "r").read()
 
 setup(
     name = "sv_ttk_colorizer",
-    version = "1.2.0",
+    version = "1.1.1",
     python_requires = ">=3.8",
     license = "MIT",
     author = "Valer100",
